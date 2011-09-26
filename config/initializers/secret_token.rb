74c39b4045530f721c6142925503bd443c2475dd
Jaroslav Kohout <kohout.jara@gmail.com>
2011-09-26 18:01:36 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DruhyProjekt::Application.config.secret_token = '7a6ad9390fe5c3dfe96ae9c6614f78067c8bc47f3e14e2ac9032c7415aa435698db3090325a0b2510688217c8392ef29c0ec69d7e947d324e711ab4bd06d55d5'
